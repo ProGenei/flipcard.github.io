4e7e4655c307e244b7b360b996cb2502b93611b7
ProGenei <69283726+ProGenei@users.noreply.github.com>
2021-01-16 07:04:21 +0300

    Create flipcard.js

---
+++ flipcard.js
@@ -0,0 +1,117 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+  const cardArray = [
+    {
+      name: 'fries',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/hotdog.png'
+    },
+    {
+      name: 'fries',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/hotdog.png'
+    }
+  ]
+
+  cardArray.sort(() => 0.5 - Math.random())
+
+  const grid = document.querySelector('.grid')
+  const resultDisplay = document.querySelector('#result')
+  let cardsChosen = []
+  let cardsChosenId = []
+  let cardsWon = []
+
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement('img')
+      card.setAttribute('src', 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/blank.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+  }
+
+  //check for matches
+  function checkForMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    
+    if(optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute('src', 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/blank.png')
+      alert('You have clicked the same image!')
+    }
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match')
+      cards[optionOneId].setAttribute('src', 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/white.png')
+      cards[optionTwoId].setAttribute('src', 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/white.png')
+      cards[optionOneId].removeEventListener('click', flipCard)
+      cards[optionTwoId].removeEventListener('click', flipCard)
+      cardsWon.push(cardsChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'https://raw.githubusercontent.com/kubowania/memory-game/master/images/blank.png')
+      alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
+      
+      resultDisplay.textContent = ' Congratulations! You found them all!'
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length ===2) {
+      setTimeout(checkForMatch, 500)
+    }
+  }
+
+  createBoard()
+})
